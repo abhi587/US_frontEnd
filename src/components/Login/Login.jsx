b9b857dae5d5f4ba67b4208c044f3b0c1ed652b7
abhi587 <abhishek.pawan14@gmail.com>
2024-11-01 20:42:43 +0530

    done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+// frontend/src/pages/Login.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-const login = () => {
+function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const { username, password } = formData;
+  const history = useHistory();
+
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5000/api/users/login', formData);
+      localStorage.setItem('token', res.data.token);
+      history.push('/');
+    } catch (error) {
+      console.error(error);
+      alert('Invalid credentials');
+    }
+  };
+
   return (
-    <div class='main'>
+    <div>
       <h1>Login</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={onChange}
+          required
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          required
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default login
+export default Login;
