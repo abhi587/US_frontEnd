467b7d40be1b1a65dbb9b98a48c43ab4d1cbc6c0
abhi587 <abhishek.pawan14@gmail.com>
2024-11-19 16:25:57 +0530

    done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+// src/components/Navbar.js
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>My Website</h1>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
